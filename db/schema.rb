c21e28ba29351e8d3dcebd40c8999ce70d0fa842
Trevor Obermann <tlo@ocsnet.com>
2015-08-29 15:04:09 -0400

    Had to switch to a postgres DB

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150829161112) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
     t.string   "company"
