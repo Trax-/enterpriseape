fd851b7b5e63420ceb35e771d5d415c13049e5f6
Trevor Obermann <tlo@ocsnet.com>
2015-09-01 16:30:07 -0400

    nested purchases

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
 
   get 'welcome/features'
 
-  resources :invoices
+  resources :invoices do
+    resources :purchases, except: [:index], controller: 'invoices/purchases'
+  end
 
   root to: 'welcome#index'
 end
