a1effe472be777d7ea1292e2c04c0f247131ba4b
Trevor Obermann <tlo@ocsnet.com>
2015-09-02 09:54:08 -0400

    Various fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902120703) do
+ActiveRecord::Schema.define(version: 20150902131849) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150902120703) do
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
     t.integer  "employee_id", limit: 4
+    t.string   "status_type", limit: 255
   end
 
   create_table "purchases", force: :cascade do |t|
